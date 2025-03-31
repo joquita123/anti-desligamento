c4939c440cf3343b9ba71e261ca1ec986c5d7e4e
Joaquim M <lulinhaverdao123@gmail.com>
2025-03-31 18:00:24 -0300

    anti-shutdown.bat

--- anti-shutdown.bat
+++ anti-shutdown.bat
@@ -2,8 +2,4 @@
 :loop
 shutdown /a
 tasklist /fi "imagename eq shutdown.exe" 2>NUL | find /i "shutdown.exe" >NUL
-if "%errorlevel%"=="0" (
-    echo Desligamento cancelado.
-    timeout /t 5 /nobreak >nul
-)
 goto loop
